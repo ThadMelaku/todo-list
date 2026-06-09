27408076d40f6952d0c0791052977275a856d07d
ThadMelaku <thad888@hotmail.com>
2026-06-09 14:28:17 -0700

    feat: implement todo and project state updates

--- src/appController.js
+++ src/appController.js
@@ -7,6 +7,11 @@ function getProjects() {
   return projects;
 }
 
+function addProject(name) {
+  const project = createProject(name);
+  projects.push(project);
+}
+
 function addTodoToProject(projectId, title, description, dueDate, priority) {
   const project = projects.find((project) => project.id === projectId);
 
@@ -16,4 +21,30 @@ function addTodoToProject(projectId, title, description, dueDate, priority) {
   project.todos.push(todo);
 }
 
-export { getProjects, addTodoToProject };
+function deleteTodo(projectId, todoId) {
+  const project = projects.find((project) => project.id === projectId);
+
+  if (!project) return;
+
+  project.todos = project.todos.filter((todo) => todo.id !== todoId);
+}
+
+function toggleTodoComplete(projectId, todoId) {
+  const project = projects.find((project) => project.id === projectId);
+
+  if (!project) return;
+
+  const todo = project.todos.find((todo) => todo.id === todoId);
+
+  if (!todo) return;
+
+  todo.completed = !todo.completed;
+}
+
+export {
+  getProjects,
+  addProject,
+  addTodoToProject,
+  deleteTodo,
+  toggleTodoComplete,
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import "./styles.css";
 
-import { getProjects, addTodoToProject } from "./appController.js";
+import {
+  getProjects,
+  addProject,
+  addTodoToProject,
+  deleteTodo,
+  toggleTodoComplete,
+} from "./appController.js";
 
-const defaultProjectId = getProjects()[0].id;
+console.log("=== Initial Projects ===");
+console.log(JSON.stringify(getProjects(), null, 2));
 
+// Test addProject
+addProject("Training Dashboard");
+
+console.log("=== After addProject ===");
+console.log(JSON.stringify(getProjects(), null, 2));
+
+// Grab references we'll need
+const defaultProject = getProjects()[0];
+const trainingProject = getProjects()[1];
+
+// Test addTodoToProject
 addTodoToProject(
-  defaultProjectId,
-  "Finish Australia trip planning",
-  "Book Flights",
-  "2026-06-10",
-  "medium"
+  trainingProject.id,
+  "Build weekly mileage chart",
+  "Display weekly totals from running data",
+  "2026-06-15",
+  "high"
 );
 
-console.log(getProjects());
+console.log("=== After addTodoToProject ===");
+console.log(JSON.stringify(getProjects(), null, 2));
+
+// Grab the newly created todo
+const todo = trainingProject.todos[0];
+
+// Test toggleTodoComplete
+toggleTodoComplete(trainingProject.id, todo.id);
+
+console.log("=== After toggleTodoComplete ===");
+console.log(JSON.stringify(getProjects(), null, 2));
+
+// Test deleteTodo
+deleteTodo(trainingProject.id, todo.id);
+
+console.log("=== After deleteTodo ===");
+console.log(JSON.stringify(getProjects(), null, 2));
